700d0cf01d8b25425f1976fdea5f2df4174e2943
thabrkci <alpacinotaha@gmail.com>
2023-10-25 12:36:35 +0300

    Patika Ders

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Patika_c_;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("***Atama Ve İşlem Atama***");
+        //Atama ve işlem atama
+        int x = 3;
+        int y = 3;
+
+        y = y+2;
+        Console.WriteLine(y);
+        y /= 1;
+        Console.WriteLine(y);
+        y += 2;
+        Console.WriteLine(y);
+        x *= 2;
+        Console.WriteLine(x);
+        
+        Console.WriteLine("***Mantiksal Operatörler***");
+        //Mantıksal Operatörler II & !
+        bool isSuccess = true;
+        bool isCompleted =false;
+
+        if(isSuccess && isCompleted)
+         Console.WriteLine("Perfect!");
+        if(isSuccess || isCompleted )
+         Console.WriteLine("Fine!");
+         if(isSuccess && !isCompleted)
+         Console.WriteLine("Great!");
+
+
+         Console.WriteLine("***İlişkisel Operatörler***");
+        //İlişkisel operatörler
+        //<, >, <=, >=, ==; !=
+        int a = 3;
+        int b = 4;
+        bool sonuc = a<b;
+
+        Console.WriteLine(sonuc);
+        sonuc = a>b;
+        Console.WriteLine(sonuc);
+        sonuc = a>=b;
+        Console.WriteLine(sonuc);
+        sonuc = a<=b;
+        Console.WriteLine(sonuc);
+        sonuc = a==b;
+        Console.WriteLine(sonuc);
+        sonuc = a!=b;
+
+        Console.WriteLine("***Aritmetik Operatörler***");
+        //Aritmetik operatörler
+        int sayi1 = 10;
+        int sayi2 = 5;
+        int sonuc1 = sayi1/sayi2;
+        Console.WriteLine(sonuc1);
+        sonuc1 = sayi1/sayi2;
+        Console.WriteLine(sonuc1);
+        sonuc1 = sayi1*sayi2;
+        Console.WriteLine(sonuc1);
+        sonuc1 = sayi1+sayi2;
+        Console.WriteLine(sonuc1);
+        sonuc1 = sayi1++;
+        Console.WriteLine(sayi1);
+        //Deneme
+
+
+
+         
+
+
+
+    }
+}
